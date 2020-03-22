22993518cec080365203062e1f8c341e1c1f0d9b
Alexander Gaidukov <alexander.gaidukov@gmail.com>
2020-03-22 15:11:57 +0300

    Use reflection to get values for validation group

--- Sources/PropertyValidator/ValidationGroup.swift
+++ Sources/PropertyValidator/ValidationGroup.swift
@@ -11,3 +11,16 @@ public protocol ValidationGroup {
     associatedtype Value
     var values: [Value?] { get }
 }
+
+extension ValidationGroup {
+    var values: [Value?] {
+        let mirror = Mirror(reflecting: self)
+        var result = [Value?]()
+        for child in mirror.children {
+            if let value = child.value as? Value? {
+                result.append(value)
+            }
+        }
+        return result
+    }
+}
